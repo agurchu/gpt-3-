1135716e1a6b42998cbed6683a384dfc66a855dd
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-27 02:33:53 +0200

    initialize

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Header,
+  Possibility,
+  WhatGPT3,
+  Features,
+} from "./containers";
+import { CTA, Brand, NavBar } from "./components";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
