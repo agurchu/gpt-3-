566c9fc96b4cead06d10186fd86b177295da0be7
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-28 13:57:29 +0200

    whatGPT3 css markup

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -10,7 +10,7 @@ const WhatGPT3 = () => {
         <Feature />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
