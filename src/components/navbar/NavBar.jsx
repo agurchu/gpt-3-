498776c78bcda0d1581b39c79fe8946de495458d
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-28 08:27:02 +0200

    assets

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/kj_dev_logo.png";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="got3__navbar-links">
+        <div className="gpt3__navbar__-lonks_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
