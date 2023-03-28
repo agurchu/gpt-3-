afb5914da152012c5568c8d3e932cd0836318b32
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-28 15:32:48 +0200

    feature and whatgpt3 jsx markup

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ text, title }) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-text">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">{text}</div>
+    </div>
+  );
 };
 
 export default Feature;
