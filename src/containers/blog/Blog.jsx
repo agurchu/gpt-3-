56a7dbb70683a8a5d68be46b993c601913edfa94
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-29 08:46:52 +0200

    blog css markup complete

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -12,14 +12,14 @@ const Blog = () => {
         </h1>
       </div>
       <div className="gpt3__blog-container">
-        <div className="gpt3__blog-conatainer_groupA">
+        <div className="gpt3__blog-container_groupA">
           <Article
             image={blog01}
             date="Sep 26, 2023"
             title="GPT3 and Open AI is the future. Let us explore how it is"
           />
         </div>
-        <div className="gpt3__blog-conatainer_groupB">
+        <div className="gpt3__blog-container_groupB">
           <Article
             image={blog02}
             date="Sep 26, 2023"
